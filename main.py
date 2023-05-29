b6d4121acb49c9611b57d0b202c579418ac821e9
jackyliu16 <18922251299@163.com>
2023-05-29 22:37:01 +0800

    add gitignore and remote pycache BC unnecessary

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import cv2
 
-from MyFunction import Run
+from MyFunction import run
 if __name__ == "__main__":
     CAM = cv2.VideoCapture(0)
     resolution = (int(CAM.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(CAM.get(cv2.CAP_PROP_FRAME_WIDTH)))
-    Run.init(resolution)
-    while True:
-        ret, img = CAM.read()
-        img = Run.run(img)
-        cv2.imshow('frame', img)
-        
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    run.init(resolution)
+    try:
+        while True:
+            ret, img = CAM.read()
+            img = run.run(img)
+            cv2.imshow('frame', img)
+            
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    except:
+        run.reset()
 
     CAM.release()
     cv2.destroyAllWindows()
