d10fbe3e4cbea5134ddad8ccbd9a43e2d648903f
jackyliu16 <18922251299@163.com>
2023-05-30 00:03:24 +0800

    rebuild function

--- MyFunction/lib.py
+++ MyFunction/lib.py
@@ -20,6 +20,7 @@ def getAreaMaxContour(contours):
         contour_area_temp = math.fabs(cv2.contourArea(c))  # 计算轮廓面积
         if contour_area_temp > contour_area_max:
             contour_area_max = contour_area_temp
+            # if contour_area_temp > 20:  # 只有在面积大于 20，最大面积的轮廓才是有效的，以过滤干扰
             area_max_contour = c
     return area_max_contour, contour_area_max  # 返回最大的轮廓
 
@@ -27,7 +28,7 @@ def getMotor(idx: int) -> int:
     """get the abs speed of motor
     """
     import HiwonderSDK.Board as Board
-    return -Board.getMotor(idx) if idx % 2 == 0 else Board.getMotor(idx)
+    return Board.getMotor(idx) if idx % 2 == 0 else -Board.getMotor(idx)
 
 def setMotor(idx: int, speed: int):
     """set motor idx into abs speed
@@ -35,7 +36,7 @@ def setMotor(idx: int, speed: int):
     import HiwonderSDK.Board as Board
     Board.setMotor(idx, speed)
 
-def setBothMotor(speed):
+def setBothMotor(speed: int):
     """setting both motor into same speed
     """
     setMotor(1, speed)
