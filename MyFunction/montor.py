a4a9f177e3824f6caa0598775dfc59ffebe2df7a
jackyliu16 <18922251299@163.com>
2023-05-29 23:00:02 +0800

    update: realize the motor method

---
+++ MyFunction/montor.py
@@ -0,0 +1,40 @@
+from HiwonderSDK.PID import PID
+from typing import *
+from MyFunction.lib import *
+from MyFunction.config import *
+
+motor_pid = PID(P=0.01, I=0.01, D=0.008)
+
+def init():
+    setBothMotor(BASE_SPEED)
+
+def move(point: Tuple[int, int]) -> None:
+    from MyFunction.run import area
+    # TODO 需要明确这个地方针对的是mon还是别的
+    # img_center_x = (area[1][0] + area[0][0]) / 2
+    img_center_y = (area[0][1] + area[1][1]) / 2
+    x, y = point
+    err = abs(img_center_y - x)
+    print(f"img[{img_center_y}], x{x}, y{y}")
+    if err < 30:
+        motor_pid.setPoint = x
+    else:
+        motor_pid.SetPoint = img_center_y
+    
+    motor_pid.update(x)
+    tmp = motor_pid.output
+    print(f"tmp: {tmp}")
+    setMotor(1, getMotor(1) - tmp * ADD_SPEED)
+    setMotor(2, getMotor(2) + tmp * ADD_SPEED)
+
+    diff = x - img_center_y
+    # print(f"diff{diff}")
+        
+
+
+
+    
+    
+    
+
+    
