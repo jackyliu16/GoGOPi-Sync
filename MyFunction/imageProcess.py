9d089c37de25daa94c27a9513afc761285346301
jackyliu16 <18922251299@163.com>
2023-05-30 00:44:20 +0800

    imageProcess::get_montoring_area: change the use of area environment

--- MyFunction/imageProcess.py
+++ MyFunction/imageProcess.py
@@ -43,9 +43,10 @@ def binarization(img: np.ndarray) -> np.ndarray:
 def get_monitoring_area(img: np.ndarray) -> np.ndarray:
     """using the area env viriable to shrink the area and return it 
     """
-    from MyFunction.run import area
-    monitoring_area = img[area[0][0]:area[1][0], area[0][1]: area[1][1]]
-    return monitoring_area
+    # from MyFunction.run import area
+    # monitoring_area = img[area[0][0]:area[1][0], area[0][1]: area[1][1]]
+    area = lib.mapping(MONITORING_AREA)
+    return img[area[0][0]:area[1][0], area[0][1]: area[1][1]]
 
 def add_mark_point(img: np.ndarray, point: Tuple[int, int]) -> np.ndarray:
     """add a mark point into image
